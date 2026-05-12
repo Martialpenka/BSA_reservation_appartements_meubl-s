bb4783334cabe68362c2b63f4b482ce76e541bf5
Martial Penka <fngho053@uottawa.ca>
2026-05-12 19:13:29 -0400

    Corriger connexion DB Railway

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,7 +10,9 @@ const pool = new Pool({
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 5000,
-    ssl: false
+   ssl: process.env.NODE_ENV === 'production' 
+    ? { rejectUnauthorized: false } 
+    : false
 });
 
 pool.on('error', (err) => {
